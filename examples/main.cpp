cc4bdaee365b05f92911a61b26f42162a5e32e43
Mona Lisa <929397876@qq.com>
2020-03-02 18:53:16 +0800

    Add tx verify file

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,10 @@
 
-//#define CKB_VM
+#define CKB_VM
+
 
 #include <cstdio>
 #include <cstdint>
-
+# include <cstring>
 
 #include "sha256.h"
 
@@ -18,8 +19,8 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        // printf("Usage: sha256 <string>\n");
-        return 0;
+        ckb_debug("argv not enough");
+        return 76;
     }
 
     char* msg = argv[1];
@@ -39,9 +40,10 @@ int main(int argc, char* argv[]) {
 
     # ifndef CKB_VM
 
-    printf("%s",format);
+    //printf("%s",format);
 
     # else
+    ckb_debug("HEllo \n");
 
     ckb_debug(format);
 
@@ -49,8 +51,11 @@ int main(int argc, char* argv[]) {
 
 
 
+    if (strcmp(format,"9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08") == 0){
+        return 0;
 
+    }
 
-    return 0;
+    return 1;
 
 }
